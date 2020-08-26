971868d6088c230da48b42e52436a3e1391678e5
David Fiel <github@dfiel.com>
2020-08-26 14:27:39 -0400

    AUgust 26 Lecture Files

---
+++ Aug26Lecture/BufferedReaderExample.java
@@ -0,0 +1,9 @@
+public class BufferedReaderExample {
+
+   public static void main(String[] args) {
+      // open the loremipsum.txt file.
+      // for each line in the file, do
+      //   - read the line as a String
+      //   - print that String to the console
+   }
+}
---
+++ Aug26Lecture/ScannerTest.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        // Find the sum of the double values contained in the file fewDoubles.txt
+
+    }
+}
